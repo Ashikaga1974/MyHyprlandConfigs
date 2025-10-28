f6fc79603068908f236c3eb3b2561baade7a4417
Ashikaga1974 <sascha.lerschen@outlook.com>
2025-10-28 15:13:44 +0100

    Added oh-my-posh theme, adjustments to fish-config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,7 @@
 set fish_greeting ""
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
 end
 
-fastfetch --logo /home/sascha/Bilder/Logo/fedora.png --logo-type kitty-direct --logo-width 33 --logo-height 15 --show-errors
-#oh-my-posh init fish | source
+fastfetch --logo ~/.config/fastfetch/fedora.png --logo-type kitty-direct --logo-width 33 --logo-height 15 --show-errors
 oh-my-posh init fish --config ~/.config/oh-my-posh/theme.json | source
