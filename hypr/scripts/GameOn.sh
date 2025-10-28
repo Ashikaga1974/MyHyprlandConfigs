31dd3654655218738909eed48fa3d13021420033
Ashikaga1974 <sascha.lerschen@outlook.com>
2025-10-28 15:15:22 +0100

    Beautyful bash improvements to codestyle

--- hypr/scripts/GameOn.sh
+++ hypr/scripts/GameOn.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "toggle" ]]; then
-  if hyprctl -j getoption "animations:enabled" | grep -q "int\": 0"; then
-    hyprctl --batch "
+    if hyprctl -j getoption "animations:enabled" | grep -q "int\": 0"; then
+        hyprctl --batch "
       keyword animations:enabled 1;
       keyword decoration:shadow:enabled 1;
       keyword decoration:blur:enabled 1;
       keyword general:gaps_in 5;
       keyword general:gaps_out 10;
       keyword general:border_size 4;
-      keyword decoration:rounding 10"
-    notify-send "Gamemode deaktiviert"
-  else
-    hyprctl --batch "
+        keyword decoration:rounding 10"
+        notify-send "Gamemode deaktiviert"
+    else
+        hyprctl --batch "
       keyword animations:enabled 0;
       keyword decoration:shadow:enabled 0;
       keyword decoration:blur:enabled 0;
       keyword general:gaps_in 0;
       keyword general:gaps_out 0;
       keyword general:border_size 1;
-      keyword decoration:rounding 0"
-    notify-send "Gamemode aktiviert"
-  fi
+        keyword decoration:rounding 0"
+        notify-send "Gamemode aktiviert"
+    fi
 fi
 
 if hyprctl -j getoption "animations:enabled" | grep -q "int\": 0"; then
-  echo '{"text":"","class":"on"}'
+    echo '{"text":"","class":"on"}'
 else
-  echo '{"text":"","class":"off"}'
+    echo '{"text":"","class":"off"}'
 fi
--- hypr/scripts/ReloadWaybar.sh
+++ hypr/scripts/ReloadWaybar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 killall waybar
-waybar -c ~/.config/hypr/waybarconf/config -s ~/.config/hypr/waybarconf/style.css & 
+waybar -c ~/.config/hypr/waybarconf/config -s ~/.config/hypr/waybarconf/style.css &
